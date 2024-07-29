c813b7bca612f7f318af79f56a8e61b6fc8ce836
samonov533 <samonov533@gmail.com>
2024-07-29 05:15:58 +0500

    first commit

---
+++ Js/main.js
@@ -0,0 +1,62 @@
+  
+  
+        var question1 = "Ismingizni kiriting"
+        var question2 = "Jadvalda bosh qator yacheykalari qaysi teg yordamida yaratiladi?"
+        var question3 = "Inputga malumot kiritib bo'lmaydigan qilish uchun qaysi atributdan foydalanamiz?"
+        var question4 = "Qaysi flex hossasi elementlarni agar sig'may qolsa pastga tushishini belgilaydi?"
+        var question5 = "HTMLda qatorni bitta pastga tushirish uchun qaysi tegdan foydalanamiz?"
+        var question6 = "Matematikadagi kvadrat HTMLda qaysi teg bilan yoziladi? "
+        var question7 = "Uzuk chiziqli border qanday yoziladi?"
+        var question8 = "HTMLda sarlavhalar qaysi teg bilan beriladi?"
+        var question9 = "text-decoration: underline qanday vazifa bajaradi?"
+        var question10 = "RGB ranglarini qaysi sonlar orqali berishimiz mumkin?"
+        var question11= "text-transform: capitalize qanday vazifa bajaradi?"
+        var question12= "ul va ol ichida qaysi teg(lar) kelishi mumkin?"
+        var question13= "Quyidagilardan qaysi biri BEM yozilish uslubi hisoblanadi?"
+        var question14= "Foydalanuvchi havolani ustiga bosgandagi stili qanday beriladi?"
+        var question15= "Qaysi position hususiyatida z-index hususiyati ishlamaydi?  "
+        var question16= "CSS ni necha xil yo'l bilan HTML faylga ulashimiz mumkin ?"
+
+ 
+
+
+        alert("Assalom alaykum , boshladikmi")        
+
+
+        var answer1 = prompt(question1)
+        var answer2 = prompt(question2)
+        var answer3 = prompt(question3)
+        var answer4 = prompt(question4)
+        var answer5 = prompt(question5)
+        var answer6 = prompt(question6)
+        var answer7 = prompt(question7)
+        var answer8 = prompt(question8)
+        var answer9 = prompt(question9)
+        var answer10 = prompt(question10)
+        var answer11 = prompt(question11)
+        var answer12 = prompt(question12)
+        var answer13 = prompt(question13)
+        var answer14 = prompt(question14)
+        var answer15 = prompt(question15)
+        var answer16 = prompt(question16)
+ 
+
+        console.log(question1 , answer1);
+        console.log(question2 , answer2);
+        console.log(question3 , answer3);
+        console.log(question4 , answer4);
+        console.log(question5 , answer5);
+        console.log(question6 , answer6);
+        console.log(question7 , answer7);
+        console.log(question8 , answer8);
+        console.log(question9 , answer9);
+        console.log(question10 , answer10);
+        console.log(question11 , answer11);
+        console.log(question12 , answer12);
+        console.log(question13 , answer13);
+        console.log(question14 , answer14);
+        console.log(question15 , answer15);
+        console.log(question16 , answer16);
+
+
+        alert("Tugadi")
